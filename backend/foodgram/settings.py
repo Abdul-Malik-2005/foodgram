81b6308d86564151eb7304997dcd4c15c7992fcc
|абдул-Малик Сугаипов <stas.basov@whatever.com>
2025-01-10 21:20:49 +0300

    Commit last

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -8,9 +8,9 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', 'SECRET')
 
-DEBUG = os.getenv("DEBUG", 'False').lower() != 'false'
+DEBUG = (os.environ.get('DEBUG') == 'True')
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '158.160.18.41', 'foodgram-smm.duckdns.org']
 
@@ -65,10 +65,10 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv('POSTGRES_DB', 'foodgram'),
-        'USER': os.getenv('POSTGRES_USER', 'foodgram_user'),
+        'NAME': os.getenv('POSTGRES_DB', 'django'),
+        'USER': os.getenv('POSTGRES_USER', 'django'),
         'PASSWORD': os.getenv('POSTGRES_PASSWORD', ''),
-        'HOST': os.getenv('DB_HOST', 'localhost'),
+        'HOST': os.getenv('DB_HOST', ''),
         'PORT': os.getenv('DB_PORT', 5432),
     }
 }
